87b315ea19d01b77af4a6d30e1574e76e2d1144d
QuantumPuls3-pixel <wissemrecham@gmail.com>
2026-04-22 19:22:17 +0100

    feat: add EasySave.Models with BackupJob, BackupState and BackupType

---
+++ EasySave.Models/BackupState.cs
@@ -0,0 +1,34 @@
+﻿public class BackupState
+{
+    public string BackupName { get; set; }
+    public string CurrentSourcePath { get; set; }
+    public string CurrentTargetPath { get; set; }
+    public bool IsRunning { get; set; }
+    public long CurrentFileSize { get; set; }
+    public DateTime LastBackupUpdateTime { get; set; }
+
+    public long TransferredBytes { get; set; }
+
+    public long TotalEligibleFileCount { get; set; }
+    public long RemainingFileCount { get; set; }
+
+    public long TotalEligibleBytes { get; set; }
+    public long RemainingBytes { get; set; }
+
+    public double Progress => TotalEligibleBytes > 0 ? (double)TransferredBytes / TotalEligibleBytes * 100 : 0;
+    public BackupState()
+    {
+        BackupName = "";
+        CurrentSourcePath = "";
+        CurrentTargetPath = "";
+        IsRunning = false;
+        CurrentFileSize = 0;
+        LastBackupUpdateTime = DateTime.MinValue;
+        TransferredBytes = 0;
+        TotalEligibleFileCount = 0;
+        RemainingFileCount = 0;
+        TotalEligibleBytes = 0;
+        RemainingBytes = 0;
+    }
+
+}
