3da57ca32e63a5932a6a0aff61b3e951b4ab16a3
Wissam <wissemrecham@gmail.com>
2026-04-22 19:32:45 +0100

    Refactor BackupState class initialization
    Refactor BackupState class to initialize properties in constructor.

--- EasySave.Models/BackupState.cs
+++ EasySave.Models/BackupState.cs
@@ -1,18 +1,29 @@
-namespace EasySave.Models
+public class BackupState
 {
-    public class BackupState
+    public string BackupName { get; set; }
+    public string CurrentSourcePath { get; set; }
+    public string CurrentTargetPath { get; set; }
+    public bool IsRunning { get; set; }
+    public long CurrentFileSize { get; set; }
+    public DateTime LastBackupUpdateTime { get; set; }
+    public long TransferredBytes { get; set; }
+    public long TotalEligibleFileCount { get; set; }
+    public long RemainingFileCount { get; set; }
+    public long TotalEligibleBytes { get; set; }
+    public long RemainingBytes { get; set; }
+    public double Progress => TotalEligibleBytes > 0 ? (double)TransferredBytes / TotalEligibleBytes * 100 : 0;
+    public BackupState()
     {
-        public string BackupName { get; set; } = string.Empty;
-        public string CurrentSourcePath { get; set; } = string.Empty;
-        public string CurrentTargetPath { get; set; } = string.Empty;
-        public bool IsRunning { get; set; }
-        public long CurrentFileSize { get; set; }
-        public DateTime LastBackupUpdateTime { get; set; }
-        public long TransferredBytes { get; set; }
-        public long TotalEligibleFileCount { get; set; }
-        public long RemainingFileCount { get; set; }
-        public long TotalEligibleBytes { get; set; }
-        public long RemainingBytes { get; set; }
-        public double Progress => TotalEligibleBytes > 0 ? (double)TransferredBytes / TotalEligibleBytes * 100 : 0;
+        BackupName = "";
+        CurrentSourcePath = "";
+        CurrentTargetPath = "";
+        IsRunning = false;
+        CurrentFileSize = 0;
+        LastBackupUpdateTime = DateTime.MinValue;
+        TransferredBytes = 0;
+        TotalEligibleFileCount = 0;
+        RemainingFileCount = 0;
+        TotalEligibleBytes = 0;
+        RemainingBytes = 0;
     }
 }
