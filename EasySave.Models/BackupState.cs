2fc176ba424ce37dd77d2fbea78f5bc3508ac89d
Wissam <wissemrecham@gmail.com>
2026-04-22 19:31:03 +0100

    Refactor BackupState class property initializations
    Refactored BackupState class to initialize properties with default values.

--- EasySave.Models/BackupState.cs
+++ EasySave.Models/BackupState.cs
@@ -1,34 +1,18 @@
-﻿public class BackupState
+namespace EasySave.Models
 {
-    public string BackupName { get; set; }
-    public string CurrentSourcePath { get; set; }
-    public string CurrentTargetPath { get; set; }
-    public bool IsRunning { get; set; }
-    public long CurrentFileSize { get; set; }
-    public DateTime LastBackupUpdateTime { get; set; }
-
-    public long TransferredBytes { get; set; }
-
-    public long TotalEligibleFileCount { get; set; }
-    public long RemainingFileCount { get; set; }
-
-    public long TotalEligibleBytes { get; set; }
-    public long RemainingBytes { get; set; }
-
-    public double Progress => TotalEligibleBytes > 0 ? (double)TransferredBytes / TotalEligibleBytes * 100 : 0;
-    public BackupState()
+    public class BackupState
     {
-        BackupName = "";
-        CurrentSourcePath = "";
-        CurrentTargetPath = "";
-        IsRunning = false;
-        CurrentFileSize = 0;
-        LastBackupUpdateTime = DateTime.MinValue;
-        TransferredBytes = 0;
-        TotalEligibleFileCount = 0;
-        RemainingFileCount = 0;
-        TotalEligibleBytes = 0;
-        RemainingBytes = 0;
+        public string BackupName { get; set; } = string.Empty;
+        public string CurrentSourcePath { get; set; } = string.Empty;
+        public string CurrentTargetPath { get; set; } = string.Empty;
+        public bool IsRunning { get; set; }
+        public long CurrentFileSize { get; set; }
+        public DateTime LastBackupUpdateTime { get; set; }
+        public long TransferredBytes { get; set; }
+        public long TotalEligibleFileCount { get; set; }
+        public long RemainingFileCount { get; set; }
+        public long TotalEligibleBytes { get; set; }
+        public long RemainingBytes { get; set; }
+        public double Progress => TotalEligibleBytes > 0 ? (double)TransferredBytes / TotalEligibleBytes * 100 : 0;
     }
-
 }
