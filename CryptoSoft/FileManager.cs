a67362a5bc796e3ecea25c60fc6aa2ee88b78193
mahmoud-mess <uzlessburner@protonmail.com>
2026-04-30 00:16:59 +0100

    chore: wire cryptosoft project packaging

---
+++ CryptoSoft/FileManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace CryptoSoft;
+
+/// <summary>
+/// File manager class
+/// This class is used to encrypt and decrypt files
+/// </summary>
+public class FileManager(string path, string key)
+{
+    private string FilePath { get; } = path;
+    private string Key { get; } = key;
+
+    /// <summary>
+    /// check if the file exists
+    /// </summary>
+    private bool CheckFile()
+    {
+        if (File.Exists(FilePath))
+            return true;
+
+        Console.WriteLine("File not found.");
+        Thread.Sleep(1000);
+        return false;
+    }
+
+    /// <summary>
+    /// Encrypts the file with xor encryption
+    /// </summary>
+    public int TransformFile()
+    {
+        if (!CheckFile()) return -1;
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        var fileBytes = File.ReadAllBytes(FilePath);
+        var keyBytes = ConvertToByte(Key);
+        fileBytes = XorMethod(fileBytes, keyBytes);
+        File.WriteAllBytes(FilePath, fileBytes);
+        stopwatch.Stop();
+        return (int)stopwatch.ElapsedMilliseconds;
+    }
+
+    /// <summary>
+    /// Convert a string in byte array
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    private static byte[] ConvertToByte(string text)
+    {
+        return Encoding.UTF8.GetBytes(text);
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="fileBytes">Bytes of the file to convert</param>
+    /// <param name="keyBytes">Key to use</param>
+    /// <returns>Bytes of the encrypted file</returns>
+    private static byte[] XorMethod(IReadOnlyList<byte> fileBytes, IReadOnlyList<byte> keyBytes)
+    {
+        var result = new byte[fileBytes.Count];
+        for (var i = 0; i < fileBytes.Count; i++)
+        {
+            result[i] = (byte)(fileBytes[i] ^ keyBytes[i % keyBytes.Count]);
+        }
+
+        return result;
+    }
+}
---
+++ CryptoSoft/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CryptoSoft;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            var fileManager = new FileManager(args[0], args[1]);
+            int ElapsedTime = fileManager.TransformFile();
+            Environment.Exit(ElapsedTime);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Environment.Exit(-99);
+        }
+    }
+}
