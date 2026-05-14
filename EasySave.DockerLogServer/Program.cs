02dfdbbe96f340aab1808371575f6653860cbf1a
Lowstt <rayanbouyacoub594@gmail.com>
2026-05-14 20:03:11 +0100

    dockerfile and DockerLogServer Project

---
+++ EasySave.DockerLogServer/Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapPost("/logs", async (HttpContext context) =>
+{
+    using var reader = new StreamReader(context.Request.Body);
+    var body = await reader.ReadToEndAsync();
+
+    Console.WriteLine("📥 Log received:");
+    Console.WriteLine(body);
+    Console.WriteLine("-----------------------------------");
+
+    return Results.Ok(new { status = "received" });
+});
+
+app.Run();
